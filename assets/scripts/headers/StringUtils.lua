45afeef00fc7459f228e1bc29610022f6d989790
indigoUan <indigoUan@gmail.com>
2022-09-10 00:03:20 +0200

    1.2.3 update

---
+++ assets/scripts/headers/StringUtils.lua
@@ -0,0 +1,13 @@
+local StringUtils = {}
+
+function StringUtils:split(inputstr, sep)
+    if sep == nil then
+       sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+       table.insert(t, str)
+    end
+    return t
+end
+
